ccb61c7b035dd04348aae4fdd4b7238ffa884bb5
Late Lee <li@latelee.org>
2018-12-27 09:45:42 +0800

    update.
    Signed-off-by: Late Lee <li@latelee.org>

--- .gitemail.js
+++ .gitemail.js
@@ -2,12 +2,13 @@
 npm i nodemailer
 使用：
 node mail.js 收件人，仓库名称，仓库地址
-
+示例：
+node .gitmail.js "li@latelee.org, latelee@163.com" "自动化CI测试" https://github.com/latelee/testci
 */
 const nodemailer  = require("nodemailer");
 
 // 参数：发件人，收件人，主题，正文（支持html格式）
-function sendMail(from, tos, subject, msg)
+function sendMail(from, aliasName, tos, subject, msg)
 {
     const smtpTransport = nodemailer.createTransport({
     host: 'smtp.exmail.qq.com',
@@ -21,8 +22,10 @@ function sendMail(from, tos, subject, msg)
     });
 
     smtpTransport.sendMail({
-        from    : from, 
-        to      : tos, //收件人邮箱，多个邮箱地址间用','隔开
+        //from    : '标题别名 <foobar@latelee.org>',
+        from    : aliasName + ' ' + '<' + from + '>',
+        //'li@latelee.org, latelee@163.com',//收件人邮箱，多个邮箱地址间用英文逗号隔开
+        to      : tos,
         subject : subject,//邮件主题
         //text    : msg,
         html    : msg
@@ -49,7 +52,7 @@ function main()
     require('child_process').exec('git log -1 --stat', function(err, stdout) {
         console.log('Last commit message on this branch is:\n', stdout);
         stdout = nl2br(stdout)
-        sendMail('test@latelee.org', args[0], 
+        sendMail('test@latelee.org', 'CI自动邮件通知', args[0], 
             args[1] + '仓库代码更新',
             '<h2>' + args[1] + '仓库代码有提交，请及时更新。</h2>地址为：<a href=\"' + args[1] + '\"target=\"_blank\">' + args[2] + '</a></br>' + '<h3>commit log:</h3>' + stdout);
     });
--- README.md
+++ README.md
@@ -3,27 +3,28 @@
 ## 功能
 当push仓库时，利用托管仓库的CI功能自动发邮件到指定人员邮件。  
 使用方法：  
-针对不同的托管仓库，使用对应的配置文件，将其整合到实际的ci脚本中。
+针对不同的托管仓库，使用对应的配置文件，将其整合到实际的ci脚本中。  
+(本项目仅是发送邮件，应属于CI的一部分)
 
 ## 实现原理
 使用nodejs的nodemailer实现发邮件功能。使用git命令获取最新提交日志，  
 并转换成html格式，作为邮件内容。  
 使用方法：
 ```
-node mail.js "li@latelee.org, latelee@163.com" "自动化CI测试" https://github.com/latelee/testci
+node mail.js "li@latelee.org" "自动化CI测试" https://github.com/latelee/testci
 ```
 第一个参数为收件人邮件，第二个参数为邮件标题，第三个参数为对应的仓库名称。  
 
-发件人邮件已有mail.js中指定。注意，本程序使用腾讯企业邮件（免费申请）并关联自有域名。  
-对于nodemailer，腾讯邮件直接使用邮箱名和密码即可，但发件人如是其它邮箱服务商，可能方法不同，  
-自行参考解决方法。
+发件人邮件已由mail.js中指定。注意，本程序使用腾讯企业邮件（免费申请）并关联自有域名。  
+对于nodemailer，腾讯企业邮件直接使用邮箱名和密码即可，但发件人如是其它邮箱服务商，  
+可能方法不同，请自行参考解决方法。
 
-**mail.js中的发件人邮箱test@latelee.org是可正常使用的，勿修改其密码。**  
+注：  
+**mail.js中的发件人邮箱test@latelee.org可正常使用，如想使用，可随意，但安全性不做任何保障。也请不要修改密码。**  
 
 ## 测试
 ### gitlab.com
-内置有ci模块。  
-配置文件：.gitlab-ci.yml
+已经内置有ci模块。写好正确的配置文件：.gitlab-ci.yml即可在push时执行。  
 
 ### github.com+travis-ci.org
 需要到travis-ci上关联github，并开启相应ci功能。  
@@ -35,3 +36,8 @@ node mail.js "li@latelee.org, latelee@163.com" "自动化CI测试" https://githu
 
 ### gitee
 不支持
+
+
+## 作者
+
+Late Lee<li@latelee.org>
